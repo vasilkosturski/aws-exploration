8e36a382b1e6db8dfdfc2145b5cb08d57f82a6fd
vasilkosturski <v.kosturski.ext@sportradar.com>
2024-01-08 08:02:44 +0200

    Initial version of news api integration

--- DataLakeFirehoseGlue/DataLakeFirehoseGlue/NewsStreamDataLakeBuilder/Program.cs
+++ DataLakeFirehoseGlue/DataLakeFirehoseGlue/NewsStreamDataLakeBuilder/Program.cs
@@ -1,31 +1,61 @@
-﻿using NewsAPI;
+﻿using System.Text;
+using System.Text.Json;
+using Amazon;
+using Amazon.KinesisFirehose;
+using Amazon.KinesisFirehose.Model;
+using NewsAPI;
 using NewsAPI.Constants;
 using NewsAPI.Models;
 
-const string apiKey = "fd294ab551504c18bed86254e42bb280";
+const string apiKey = "your_api_key";
+const string firehoseStream = "NEWS-API-STREAM";
 
-await FetchNewsAsync();
+var articles = await FetchNewsAsync();
 
-static async Task FetchNewsAsync()
+foreach (var article in articles)
+    Console.WriteLine($"{article.PublishedAt}: {article.Title}");
+
+using var firehoseClient = new AmazonKinesisFirehoseClient(RegionEndpoint.USEast1);
+await SendToFirehoseAsync(firehoseClient, articles);
+
+Console.ReadKey();
+
+static async Task<List<Article>> FetchNewsAsync()
 {
     var newsApiClient = new NewsApiClient(apiKey);
     var articlesResponse = await newsApiClient.GetEverythingAsync(new EverythingRequest
     {
         Sources = new List<string>
         {
-            "bbc-news", "abc-news", "associated-press", "bbc-sport", "business-insidert", "cbs-news", "cnn",
+            "bbc-news", "abc-news", "associated-press", "bbc-sport", "business-insider", "cbs-news", "cnn",
             "google-news", "techradar"
         },
         SortBy = SortBys.PublishedAt,
         Language = Languages.EN,
         From = DateTime.UtcNow.Subtract(TimeSpan.FromDays(10))
     });
-    if (articlesResponse.Status == Statuses.Ok)
+    
+    return articlesResponse.Articles;
+}
+
+static async Task SendToFirehoseAsync(AmazonKinesisFirehoseClient firehoseClient, List<Article> articles)
+{
+    foreach (var article in articles)
     {
-        foreach (var article in articlesResponse.Articles)
+        var articleJson = JsonSerializer.Serialize(article); 
+        
+        var record = new Record
         {
-            Console.WriteLine(article.PublishedAt);
-        }
+            Data = new MemoryStream(Encoding.UTF8.GetBytes($"{articleJson}\n"))
+        };
+
+        var request = new PutRecordRequest
+        {
+            DeliveryStreamName = firehoseStream,
+            Record = record
+        };
+
+        var response = await firehoseClient.PutRecordAsync(request);
+        Console.WriteLine($"Sent to Firehose, Record ID: {response.RecordId}");
     }
-    Console.ReadLine();
 }
