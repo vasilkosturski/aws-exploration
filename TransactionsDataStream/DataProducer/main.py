ee7c6d356e4664c6fbf22fdd3ef1b13cf12fbb2f
vasilkosturski <v.kosturski.ext@sportradar.com>
2024-03-22 08:55:54 +0200

    Added data producer

---
+++ TransactionsDataStream/DataProducer/main.py
@@ -0,0 +1,48 @@
+import json
+import boto3
+from datetime import datetime, timedelta
+
+STREAM_NAME = 'TransactionsDataStream'
+
+kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+
+start_time = datetime.now()
+
+
+def increment_time(minutes=0, seconds=0):
+    return start_time + timedelta(minutes=minutes, seconds=seconds)
+
+
+# Transactions with incremented timestamps
+transactions = [
+    # Normal transactions
+    {'accountId': 'acc123', 'amount': 50, 'eventTime': increment_time(minutes=15)},
+    {'accountId': 'acc123', 'amount': 75, 'eventTime': increment_time(minutes=30)},
+    {'accountId': 'acc123', 'amount': 20, 'eventTime': increment_time(minutes=45)},
+    {'accountId': 'acc123', 'amount': 100, 'eventTime': increment_time(minutes=60)},
+    {'accountId': 'acc123', 'amount': 65, 'eventTime': increment_time(minutes=75)},
+    {'accountId': 'acc123', 'amount': 120, 'eventTime': increment_time(minutes=105)},
+    {'accountId': 'acc123', 'amount': 30, 'eventTime': increment_time(minutes=120)},
+    {'accountId': 'acc123', 'amount': 110, 'eventTime': increment_time(minutes=150)},
+    # Non-fraudulent transaction pair with a big time gap
+    {'accountId': 'acc123', 'amount': 5, 'eventTime': increment_time(minutes=180)},
+    {'accountId': 'acc123', 'amount': 1000, 'eventTime': increment_time(minutes=195)},
+    # Fraudulent transaction pair with a small time gap
+    {'accountId': 'acc123', 'amount': 5, 'eventTime': increment_time(minutes=240)},
+    {'accountId': 'acc123', 'amount': 1500, 'eventTime': increment_time(minutes=240, seconds=30)},
+]
+
+
+def send_transaction(transaction):
+    transaction['eventTime'] = transaction['eventTime'].isoformat()  # Convert to ISO format
+    result = kinesis_client.put_record(
+        StreamName=STREAM_NAME,
+        Data=json.dumps(transaction),
+        PartitionKey=transaction['accountId']
+    )
+    print(f"Sent transaction for amount ${transaction['amount']} at {transaction['eventTime']}. Sequence number: {result['SequenceNumber']}")
+
+
+if __name__ == '__main__':
+    for transaction in transactions:
+        send_transaction(transaction)
