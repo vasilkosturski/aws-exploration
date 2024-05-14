5d51f738d20c042b62a356e3e4af72ddc09acfb5
vasilkosturski <v.kosturski.ext@sportradar.com>
2024-05-14 08:51:43 +0300

    Use Sink<String>

--- TransactionsDataStream/FraudDetection/FraudDetection/src/main/java/frauddetection/FraudDetector.java
+++ TransactionsDataStream/FraudDetection/FraudDetection/src/main/java/frauddetection/FraudDetector.java
@@ -2,10 +2,12 @@ package frauddetection;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.connector.kinesis.sink.KinesisStreamsSink;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
@@ -13,37 +15,23 @@ import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConsta
 import java.util.Properties;
 
 public class FraudDetector {
-    private static final String region = "us-east-1";
-    private static final String inputStreamName = "TransactionsInputStream";
-    private static final String outputStreamName = "TransactionsOutputStream";
 
+    private final SourceFunction<String> source;
+    private final Sink<String> sink;
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private static DataStream<Transaction> createSource(StreamExecutionEnvironment env) {
-        Properties inputProperties = new Properties();
-        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
-        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
+    public FraudDetector(SourceFunction<String> source, Sink<String> sink) {
+        this.source = source;
+        this.sink = sink;
+    }
 
-        return env.addSource(new FlinkKinesisConsumer<>(inputStreamName, new SimpleStringSchema(), inputProperties))
+    private static DataStream<Transaction> createSource(StreamExecutionEnvironment env, SourceFunction<String> sourceFunction) {
+        return env.addSource(sourceFunction)
                 .map((MapFunction<String, Transaction>) value -> mapper.readValue(value, Transaction.class));
     }
 
-    private static KinesisStreamsSink<String> createSink() {
-        Properties outputProperties = new Properties();
-        outputProperties.setProperty(AWSConfigConstants.AWS_REGION, region);
-
-        return KinesisStreamsSink.<String>builder()
-                .setKinesisClientProperties(outputProperties)
-                .setSerializationSchema(new SimpleStringSchema())
-                .setStreamName(outputStreamName)
-                .setPartitionKeyGenerator(element -> String.valueOf(element.hashCode()))
-                .build();
-    }
-
-    public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        DataStream<Transaction> transactions = createSource(env);
+    public void build(StreamExecutionEnvironment env) throws Exception {
+        DataStream<Transaction> transactions = createSource(env, source);
 
         DataStream<Alert> alerts = transactions
                 .keyBy(Transaction::getAccountId)
@@ -53,8 +41,28 @@ public class FraudDetector {
         DataStream<String> alertStrings = alerts.map((MapFunction<Alert, String>) alert ->
                 mapper.writeValueAsString(alert));
 
-        alertStrings.sinkTo(createSink());
+        alertStrings.sinkTo(sink);
 
-        env.execute("Enhanced Fraud Detection with Kinesis Integration");
+        env.execute("Enhanced Fraud Detection with Generic Integration");
+    }
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties inputProperties = new Properties();
+        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, "us-east-1");
+        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
+        FlinkKinesisConsumer<String> consumer = new FlinkKinesisConsumer<>("TransactionsInputStream", new SimpleStringSchema(), inputProperties);
+
+        Properties outputProperties = new Properties();
+        outputProperties.setProperty(AWSConfigConstants.AWS_REGION, "us-east-1");
+        KinesisStreamsSink<String> sink = KinesisStreamsSink.<String>builder()
+                .setKinesisClientProperties(outputProperties)
+                .setSerializationSchema(new SimpleStringSchema())
+                .setStreamName("TransactionsOutputStream")
+                .setPartitionKeyGenerator(element -> String.valueOf(element.hashCode()))
+                .build();
+
+        new FraudDetector(consumer, sink).build(env);
     }
 }
